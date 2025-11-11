05c946f2f6796850b6ecc48456ee654ae581a3a8
tutruong8 <travistu8@gmail.com>
2025-11-11 13:33:08 -0500

    updated info on education, work experience, and links to things including projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function ($) {
 
 
 		/*======= Populate GitHub Projects *=======*/
-		let gitHubUsername = 'YOUR GITHUB USERNAME';
+		let gitHubUsername = 'tutruong8';
 
 		// Run GitHub API function, passing in the GitHub username
 		requestUserRepos(gitHubUsername)
